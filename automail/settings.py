e3dedf934c7c797ad468dc97ca708ecedc8b86c3
Taterbro <veecipher@gmail.com>
2023-12-29 17:34:47 +0100

    hid email

--- automail/settings.py
+++ automail/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ciphertorgames@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxdp xegc zryy haep'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
